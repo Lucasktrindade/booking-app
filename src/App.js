d2ff08d5a55e1560439b1b455d9f02952bb33c09
Lucas Trindade <lucasktrindade@gmail.com>
2018-09-22 18:50:33 -0300

    :art: Adiciona componentes no App para montar Home

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HouseSearch from './components/house_search.js';
+import HouseList from './components/house_list.js';
+import './assets/App.css';
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      houses: [
+        {thumbnail: 'https://via.placeholder.com/120x120', name: 'Título da Casa', address: 'Localização da Casa', rate: 'Melhor Lugar para Visitar'},
+        {thumbnail: 'https://via.placeholder.com/120x120', name: 'Título da Casa', address: 'Localização da Casa', rate: 'Melhor Lugar para Visitar'},
+        {thumbnail: 'https://via.placeholder.com/120x120', name: 'Título da Casa', address: 'Localização da Casa', rate: 'Melhor Lugar para Visitar'},
+        {thumbnail: 'https://via.placeholder.com/120x120', name: 'Título da Casa', address: 'Localização da Casa', rate: 'Melhor Lugar para Visitar'},
+        {thumbnail: 'https://via.placeholder.com/120x120', name: 'Título da Casa', address: 'Localização da Casa', rate: 'Melhor Lugar para Visitar'},
+        {thumbnail: 'https://via.placeholder.com/120x120', name: 'Título da Casa', address: 'Localização da Casa', rate: 'Melhor Lugar para Visitar'}
+      ]
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container mx-md mx-auto">
+        <h1 className="text-center mt-6 mb-6">HouserGuest</h1>
+        <HouseSearch/>
+        <HouseList houses={this.state.houses}/>
       </div>
     );
   }
