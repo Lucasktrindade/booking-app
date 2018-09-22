7657c49d666ace3d38ea4a93fdaee48f706d280e
Lucas Trindade <lucasktrindade@gmail.com>
2018-09-22 18:48:43 -0300

    :sparkles: Adiciona componente HouseSearch

---
+++ src/components/house_search.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HouseSearch = (props) => {
+	return <input className="shadow border rounded text-grey-darker p-2 mt-6 mb-6 w-4/5 md:w-2/5 block mr-auto ml-auto" type = "text" placeholder = "Onde quer se hospedar?" />
+}
+
+export default HouseSearch
