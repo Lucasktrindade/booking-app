e96379928461338bb192832bf9442eb8c7a69e27
Lucas Trindade <lucasktrindade@gmail.com>
2018-09-24 10:38:02 -0300

    :building_construction: Adiciona rotas na aplicação

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore, applyMiddleware  } from 'redux';
+import { Provider } from 'react-redux';
+import promise from 'redux-promise'
+import {BrowserRouter, Route} from 'react-router-dom';
+import HouseHome from './containers/house_home.js';
+import HouseDetail from './containers/house_detail.js';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from "./reducers";
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={createStore(reducers, applyMiddleware(promise))}>
+    <BrowserRouter>
+      <div>
+        <Route path="/house/:id" component={HouseDetail} />
+        <Route exact path="/" component={HouseHome} />
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
