9b08880c07653edf26bfc080fec7581f165f766b
Lucas Trindade <lucasktrindade@gmail.com>
2018-09-25 03:05:46 -0300

    :sparkles: Adiciona link na listagem do router

--- src/containers/house_list.js
+++ src/containers/house_list.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom'
 import HouseItem from '../components/house_item';
 
 class HouseList extends Component {
@@ -9,7 +10,7 @@ class HouseList extends Component {
 	render () {
 		return (
 	      <div className="flex mt-10 mb-6 flex-wrap justify-around">
-	        {this.props.houses.map((house, index) => <HouseItem house={house} key={index}/>)}
+	        {this.props.houses.map((house, index) =>  <Link className="no-underline text-black hover:text-blue-darker" to={`house/${index+1}`}><HouseItem house={house} key={index}/></Link>)}
 	      </div>
 			)
 	}
