2fe4dd76a361e2cc7cdb0524b81e839f909dab4f
Lucas Trindade <lucasktrindade@gmail.com>
2018-12-15 02:03:01 -0200

    :sparkles: Lazy load na rota de detalhe

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware  } from 'redux';
 import { Provider } from 'react-redux';
 import promise from 'redux-promise'
 import {BrowserRouter, Route} from 'react-router-dom';
 import HouseHome from './containers/house_home.js';
-import HouseDetail from './containers/house_detail.js';
 import registerServiceWorker from './registerServiceWorker';
 import reducers from "./reducers";
 import './index.css';
 
+
+const HouseDetail = lazy(() => import('./containers/house_detail.js'));
+
 ReactDOM.render(
   <Provider store={createStore(reducers, applyMiddleware(promise))}>
     <BrowserRouter>
-      <div>
-        <Route path="/house/:id" component={HouseDetail} />
+      <Suspense fallback={<div>Loading...</div>}>
         <Route exact path="/" component={HouseHome} />
-      </div>
+        <Route path="/house/:id" component={HouseDetail} />
+      </Suspense>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
